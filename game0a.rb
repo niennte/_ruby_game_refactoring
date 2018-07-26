18c863ea98ddec080752bba37c6799d440306745
zencam@imac <zen.camera.project@gmail.com>
2018-07-26 13:18:26 -0400

    Gardening game from ground zero, before refactoring.

---
+++ game0a.rb
@@ -0,0 +1,2 @@
+puts "What would you like to do? (\"q\": quit, \"p\": plant, \"w\": water, \"x\": weed, \"r\": rest)"
+action = gets.chomp
---
+++ game0b.rb
@@ -0,0 +1,4 @@
+while true
+  puts "What would you like to do? (\"q\": quit, \"p\": plant, \"w\": water, \"x\": weed, \"r\": rest)"
+  puts "You chose #{gets.chomp}"
+end
